436833bd799eb8c74ff40c84426dfbe13c35d646
Misael <101156132+Misaelsharkbot@users.noreply.github.com>
2022-05-23 07:16:05 -0500

    Update propietario(a)-restart.js

--- plugins/propietario(a)-restart.js
+++ plugins/propietario(a)-restart.js
@@ -1,4 +1,4 @@
-import { spawn } from 'child_process'
+/*import { spawn } from 'child_process'
 let handler = async (m, { conn, isROwner, text }) => {
     if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
     if (global.conn.user.jid == conn.user.jid) {
@@ -17,3 +17,4 @@ handler.exp = 500
 handler.rowner = true
 
 export default handler
+*/
