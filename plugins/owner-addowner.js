ecac4138563b90d6e9fa6c13caa9d613c9a9f0f4
Misael <101156132+Misaelsharkbot@users.noreply.github.com>
2022-05-23 10:21:04 -0500

    Update owner-addowner.js

--- plugins/owner-addowner.js
+++ plugins/owner-addowner.js
@@ -5,10 +5,10 @@ let handler = async (m, { conn, args }) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : args[0].replace(/[^0-9]/g, '') + '@s.whatsapp.net'
     else who = args[0] ? args[0].replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
-    if (json.includes(who.split`@`[0])) throw `${await conn.getName(who)} sudah jadi owner!`
+    if (json.includes(who.split`@`[0])) throw `${await conn.getName(who)} ya es owner`
     json.push(`${who.split`@`[0]}`)
     fs.writeFileSync('./src/owner.json', JSON.stringify(json))
-    m.reply(`${await conn.getName(who)} sekarang jadi owner!`)
+    m.reply(`${await conn.getName(who)} ahora eres owner`)
 
     delete require.cache[require.resolve('../config')]
     require('../config')
