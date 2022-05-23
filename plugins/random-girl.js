f1ff442162f60475f2ed3b6a5db005ecb1b57e09
Misael <101156132+Misaelsharkbot@users.noreply.github.com>
2022-05-22 22:17:32 -0500

    Update random-girl.js

--- plugins/random-girl.js
+++ plugins/random-girl.js
@@ -6,7 +6,7 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 	let girl = (await axios.get(`https://raw.githubusercontent.com/FG98F/team-fg/main/img/girl.json`)).data
   
 //await conn.sendFile(m.chat, pickRandom(girl), 'girl.jpg', ✅ Resultado 🤭', m)
-await conn.sendButton(m.chat, '✅ Resultado 🤭', 'Click en siguiente para ir a la siguiente imagen', pickRandom(girl), [['▷▷ SIGUIENTE', `${usedPrefix + command}`]],m)
+await conn.sendButton(m.chat, '✅ Resultado 🤭', 'Click en siguiente para ir a la siguiente imagen', pickRandom(girl), [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `${usedPrefix + command}`]],m)
 }
 handler.help = ['girl']
 handler.tags = ['img']
