fbc4897e70a4dd79f20291f65375faf73d930ab9
Misael <101156132+Misaelsharkbot@users.noreply.github.com>
2022-05-24 10:47:18 -0500

    Update cmd-lock.js

--- plugins/cmd-lock.js
+++ plugins/cmd-lock.js
@@ -1,16 +1,13 @@
-module.exports = Object.assign(async function handler(m, { isOwner, isPremium, command }) {
-    if (!(isOwner || isPremium)) {
-        global.dfail('premium', m, conn)
-        throw false
-    }
-    if (!m.quoted) throw '¡Mensaje de respuesta!'
-    if (!m.quoted.fileSha256) throw 'Falta hash SHA256'
-    let sticker = global.db.data.sticker
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+    if (!m.quoted) throw 'Responde a un mensaje'
+    if (!m.quoted.fileSha256) throw 'SHA256 Hash Missing'
+    let sticker = db.data.sticker
     let hash = m.quoted.fileSha256.toString('hex')
-    if (!(hash in sticker)) throw 'Hash no encontrado en la base de datos'
+    if (!(hash in sticker)) throw 'No está registrado en la base de datos'
     sticker[hash].locked = !/^un/i.test(command)
-    m.reply('¡Hecho!')
-}
+    m.reply('Hecho')
+} 
 handler.help = ['un', ''].map(v => v + 'lockcmd')
 handler.tags = ['database']
 handler.command = ['unlockcmd', 'lockcmd'] 
