b26e83e90422092a6241a6a8e9f8233c2cd053b9
Misael <101156132+Misaelsharkbot@users.noreply.github.com>
2022-05-24 13:51:08 -0500

    Update rpg-perfil.js

--- plugins/rpg-perfil.js
+++ plugins/rpg-perfil.js
@@ -9,18 +9,24 @@ pp = await conn.getProfilePicture(who)
 } catch (e) {
 
 } finally {
-let { name, limit, lastclaim, registered, regTime, age } = global.db.data.users[who]
+let { name, limit, lastclaim, registered, regTime, age, role, exp  } = global.db.data.users[who]
 let username = conn.getName(who)
 let prem = global.prems.includes(who.split`@`[0])
 let sn = createHash('md5').update(who).digest('hex')
-let str = `*𝙽𝙾𝙼𝙱𝚁𝙴:* ${username} ${registered ? '(' + name + ') ': ''}
-*𝙽𝚄𝙼𝙴𝚁𝙾:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
-*𝙻𝙸𝙽𝙺:* wa.me/${who.split`@`[0]}${registered ? '\n*𝙴𝙳𝙰𝙳:* ' + age + ' años' : ''}
-*𝙻𝙸𝙼𝙸𝚃𝙴:* ${limit} 𝚄𝚂𝙾𝚂
-*𝚁𝙴𝙶𝙸𝚂𝚃𝚁𝙰𝙳𝙾:* ${registered ? 'Si': 'No'}
-*𝙿𝚁𝙴𝙼𝙸𝚄𝙼:* ${prem ? 'Si' : 'No'}
-*𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝚂𝙴𝚁𝙸𝙴:* 
-${sn}`
+let str = `
+╭───ꕥ *𝙿𝙴𝚁𝙵𝙸𝙻* ꕥ───✾
+│
+│•> *𝙽𝙾𝙼𝙱𝚁𝙴 :* ${username} ${registered ? '(' + name + ') ': ''}
+│•> *𝙽𝚄𝙼𝙴𝚁𝙾 :* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
+│•> *𝙻𝙸𝙽𝙺   :* wa.me/${who.split`@`[0]}${registered ? '\n*𝙴𝙳𝙰𝙳:* ' + age + ' años' : ''}
+│•> *𝚁𝙰𝙽𝙶𝙾  :* *${role}*
+│•> *𝙻𝙸𝙼𝙸𝚃𝙴 :* ${limit} 𝚄𝚂𝙾𝚂
+│•> *𝙴𝚇𝙿    :* *${exp}*
+│•> *𝚁𝙴𝙶𝙸𝚂𝚃𝚁𝙰𝙳𝙾:* ${registered ? 'Si': 'No'}
+│•> *𝙿𝚁𝙴𝙼𝙸𝚄𝙼:* ${prem ? 'Si' : 'No'}
+│•> *𝙱𝙰𝙽𝙴𝙰𝙳𝙾:* No
+│•> *𝙱𝙻𝙾𝚀𝚄𝙴𝙰𝙳𝙾:* No
+╰─────────────────────`
 conn.sendButton(m.chat, str, author, await(await fetch(pp)).buffer(), [['𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '/menu']], m)
 }}
 handler.help = ['profile [@user]']
