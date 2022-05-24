c52af9c860179a6ebae2dbf88bdb398b4d58e7d5
Misael <101156132+Misaelsharkbot@users.noreply.github.com>
2022-05-24 09:41:21 -0500

    Create bienvedina.js

---
+++ plugins/bienvedina.js
@@ -0,0 +1,9 @@
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/096776ca165d85888ebd6.png', m, { packname: "welcome", author: "ʕ•́ᴥ•̀ʔっ" })
+}
+
+handler.customPrefix = /^(bienvenido|bienvenidad)$/i
+handler.command = new RegExp
+
+module.exports = handler
