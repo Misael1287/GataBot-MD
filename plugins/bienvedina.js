9d2e4ba350d0287460804a2d6dac6c0838f12c80
Misael <101156132+Misaelsharkbot@users.noreply.github.com>
2022-05-24 13:04:41 -0500

    Delete bienvedina.js

--- plugins/bienvedina.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png', m, { packname: "welcome", author: "ʕ•́ᴥ•̀ʔっ" })
-}
-
-handler.customPrefix = /^(bienvenido|bienvenidad)$/i
-handler.command = new RegExp
-
-export default handler
