8483f7eba00871fb8871dd2f67e5fcfdedde3e11
Misael <101156132+Misaelsharkbot@users.noreply.github.com>
2022-05-24 12:34:59 -0500

    Update bienvedina.js

--- plugins/bienvedina.js
+++ plugins/bienvedina.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/096776ca165d85888ebd6.png', m, { packname: "welcome", author: "ʕ•́ᴥ•̀ʔっ" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png', m, { packname: "welcome", author: "ʕ•́ᴥ•̀ʔっ" })
 }
 
 handler.customPrefix = /^(bienvenido|bienvenidad)$/i
