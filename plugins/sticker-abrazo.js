c67775c375db68dc5a52903dced8c241cb33980b
Misael <101156132+Misaelsharkbot@users.noreply.github.com>
2022-05-25 07:37:26 -0500

    Update sticker-abrazo.js

--- plugins/sticker-abrazo.js
+++ plugins/sticker-abrazo.js
@@ -7,8 +7,8 @@ if(m.quoted?.sender) m.mentionedJid.push(m.quoted.sender)
 if(!m.mentionedJid.length) m.mentionedJid.push(m.sender)
 let res = await fetch('https://some-random-api.ml/animu/hug')
 let json = await res.json()
-let { url } = json
-let stiker = await sticker(null, url, `+${m.sender.split('@')[0]} le dio un abrazo a ${m.mentionedJid.map((user)=>(user === m.sender)? 'alguien ': `+${user.split('@')[0]}`).join(', ')}`)
+let { link } = json
+let stiker = await sticker(null, link, `+${m.sender.split('@')[0]} le dio un abrazo a ${m.mentionedJid.map((user)=>(user === m.sender)? 'alguien ': `+${user.split('@')[0]}`).join(', ')}`)
 conn.sendFile(m.chat, stiker, null, { asSticker: true })
 } catch (e) { }}
 handler.command = /^(abrazo|hug|consolar)$/i
