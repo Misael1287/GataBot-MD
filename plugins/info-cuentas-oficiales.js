5423b9f97561846d6b1b2723fc314b64aff653b0
Misael <101156132+Misaelsharkbot@users.noreply.github.com>
2022-05-25 16:00:03 -0500

    Update info-cuentas-oficiales.js

--- plugins/info-cuentas-oficiales.js
+++ plugins/info-cuentas-oficiales.js
@@ -13,14 +13,13 @@ let str = `
 ✅ *YOUTUBE*
 *${yt}*
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-*Si tienen dudas, sugerencias, o preguntas solo escriban por Instagram.*\n
-*If you have doubts, suggestions or questions just write on Instagram.*
+*Si tienen dudas, sugerencias, o preguntas solo escriban por Instagram.*
 `.trim()
   
 conn.sendHydrated(m.chat, str, wm, media, 'https://github.com/GataNina-Li/GataBot-MD', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
-['𝙂𝙧𝙪𝙥𝙤𝙨 𝙊𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 | 𝙂𝙧𝙤𝙪𝙥𝙨 🔰', '.grupos'],
-['𝘾𝙧𝙚𝙖𝙙𝙤𝙧𝙖 | 𝘾𝙧𝙚𝙖𝙩𝙤𝙧 💗', '#owner'],
-['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
+['𝙂𝙧𝙪𝙥𝙤𝙨 𝙊𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 🔰', '.grupos'],
+['𝘾𝙧𝙚𝙖𝙙𝙤𝙧𝙖 💗', '#owner'],
+['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́☘️', '/menu']
 ], m,)}
 
 handler.command = /^cuentasoficiales|gataig|cuentasgb|cuentagb|accounts|gataaccounts|account|iggata|cuentasdegata|cuentasdegatabot|cuentagatabot|cuentasgatabot$/i
